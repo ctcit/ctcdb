70683019b9632b4eb0edcce93fff53bfc521394a
Nick Edwards <nickedwrds@gmail.com>
2024-03-23 15:32:25 +1300

    Tidy up hut door codes

--- app/Controllers/Rest/Hutdoorcodes.php
+++ app/Controllers/Rest/Hutdoorcodes.php
@@ -16,7 +16,9 @@ class HutDoorCodes extends BaseResourceController
 
     // The HutDoorCodesModel allows for an arbitrary number of code records
     // Here we only ever return the current code and the next future code,
-    // and only allow one future code to be set at a time - and we delete any other future codes
+    // and only allow one future code to be set at a time - and we delete any other future codes.
+    // We intentionally only support GET (index) and POST (create) here - editing and deleting are only
+    // supported but POSTing a new record.
 
     /**
      * Constructor.
@@ -42,30 +44,12 @@ class HutDoorCodes extends BaseResourceController
         $future = $this->model->future();
         $result = [
             "current_code" => $current->code,
-            "future_code" => $future[0]->code,
-            "future_effective" => $future[0]->effective
+            "future_code" => isset($future[0]) ? $future[0]->code : '',
+            "future_effective" => isset($future[0]) ? $future[0]->effective : ''
         ];
         return $this->respond($result);
     }
 
-    /*
-    public function create()
-    {
-        if (!$this->isAdmin()) {
-            return $invalidResponse;
-        }
-        $data = $this->getData();
-        $entry = new \App\Models\HutDoorCode($data);
-        $result = $this->model->tryAdd($entry, 2218);
-        if ($result['result'] != "OK")
-        {
-            return $this->respond(["status"=>"failed", "reason"=>$result['result']], 400);
-        }
-        return $this->respond($result['codeEntry']);
-    }
-    */
-
-    // We (soft) delete alll existing records and add new ones
     public function create()
     {
         if (!$this->isAdmin()) {
@@ -73,19 +57,18 @@ class HutDoorCodes extends BaseResourceController
         }
         // Delete all existing entries - if they don't match the new ones
         $data = $this->request->getJSON();
-        if (!isset($data->code) || !isset($data->future_code) || !isset($data->future_effective)) {
-            print_r($data);
+        if (!isset($data->current_code) || !isset($data->future_code) || !isset($data->future_effective)) {
             return $this->respond("Missing data", 400);
         }
         $existing = $this->model->current();
-        if ($existing && $existing->code != $data->code) {
+        if ($existing && $existing->code != $data->current_code) {
             $this->model->delete($existing->id);
-            $entry = new \App\Models\DoorCode(["effective"=>date("Y-m-d"), "code"=>$data->code  ]);
+            $entry = new \App\Models\DoorCode(["effective"=>date("Y-m-d"), "code"=>$data->current_code  ]);
             $this->model->save($entry);
         }
         $future = $this->model->future();
+        $exists = false;
         foreach($future as $f) {
-            $exists = false;
             if ($f->code == $data->future_code && $f->effective == $data->future_effective) {
                 $exists = true;
             } else {
